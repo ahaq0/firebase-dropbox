06740d99035944069b02fe55655eded8c5ff9955
ahaq <ahaq@axon.com>
2024-10-25 00:23:55 -0700

    darken dropzone

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -12,10 +12,12 @@ import {
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { useState } from "react";
 import DropzoneComponent from "react-dropzone";
+
 function Dropzone() {
   const maxSize = 20971520; //20MB max
   const [loading, setLoading] = useState(false);
   const { isLoaded, isSignedIn, user } = useUser();
+
   const onDrop = (acceptedFile: File[]) => {
     acceptedFile.forEach((file) => {
       const reader = new FileReader();
@@ -28,6 +30,7 @@ function Dropzone() {
       reader.readAsArrayBuffer(file);
     });
   };
+
   const uploadFile = async (selectedFile: File) => {
     if (loading) return;
     if (!user) return;
@@ -50,6 +53,7 @@ function Dropzone() {
     });
     setLoading(false);
   };
+
   return (
     <DropzoneComponent minSize={0} maxSize={maxSize} onDrop={onDrop}>
       {({
@@ -68,8 +72,8 @@ function Dropzone() {
               className={cn(
                 "w-full h-52 flex justify-center items-center p-5 border border-dashed rounded-lg text-center",
                 isDragActive
-                  ? "bg-[#035ffe] text-white animate-pulse"
-                  : "bg-slate-100/50 dark:bg-slate=800/80 text-slate-400"
+                  ? "bg-[#0051CC] text-white animate-pulse"
+                  : "bg-[#1A1D23] dark:bg-[#2B2F3A] text-gray-300"
               )}
             >
               <input {...getInputProps()} />
@@ -77,7 +81,7 @@ function Dropzone() {
               {isDragActive && !isDragReject && "Drop to upload this file!"}
               {isDragReject && "File type not accepted, sorry!!"}
               {isFileTooLarge && (
-                <div className="text-danger mt-2">File is too large</div>
+                <div className="text-red-500 mt-2">File is too large</div>
               )}
             </div>
           </section>
