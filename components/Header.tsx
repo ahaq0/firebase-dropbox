9af5d7f67aaee2613b2400ad0e4ec1cce2847041
ahaq <ahaq@axon.com>
2024-10-29 22:26:22 -0700

    add padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,16 +8,9 @@ function Header() {
   return (
     <header className="flex items-center justify-between">
       <Link href="/" className="flex items-center space-x-2">
-      <div className="bg-[#f3f6f4] w-fit p-2 rounded">
-  <Image
-    src="https://www.shareicon.net/data/512x512/2016/05/06/760789_ai_512x512.png"
-    alt="logo"
-    height={20}
-    width={20}
-  />
-</div>
+        {/* <h1 className="texy-bold font-xl">Atlas AI</h1> */}
+        <h1 className="text-bold text-xl px-4 py-2">Atlas AI</h1>
 
-        <h1 className="texy-bold font-xl">Atlas AI</h1>
       </Link>
       <div className="px-5 flex space-x-2 items-center">
         {/* <ThemeToggler/> */}
